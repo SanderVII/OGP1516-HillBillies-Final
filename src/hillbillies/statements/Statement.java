fe600c5cb620fab12bcbeb661cd13276ea13284e
Thomas <thomas.vranken@hotmail.com>
2016-05-10 15:19:44 +0200

    Implemented statement+workat constructor

--- src/hillbillies/statements/Statement.java
+++ src/hillbillies/statements/Statement.java
@@ -8,6 +8,19 @@ import hillbillies.part3.programs.SourceLocation;
 
 public abstract class Statement {
 	
+	public Statement(SourceLocation sourceLocation) {
+		this.setSourceLocation(sourceLocation);
+	}
+	
 	public abstract void execute(World world, Unit unit,List<int[]> selectedCubes, SourceLocation sourceLocation);
-
+	
+	public SourceLocation getSourceLocation() {
+		return this.sourceLocation;
+	}
+	
+	public void setSourceLocation(SourceLocation sourceLocation) {
+		this.sourceLocation = sourceLocation;
+	}
+	
+	private SourceLocation sourceLocation;
 }
--- src/hillbillies/statements/WorkAtStatement.java
+++ src/hillbillies/statements/WorkAtStatement.java
@@ -2,16 +2,34 @@ package hillbillies.statements;
 
 import java.util.List;
 
+import hillbillies.expressions.*;
 import hillbillies.model.Unit;
 import hillbillies.model.World;
 import hillbillies.part3.programs.SourceLocation;
 
-public class WorkAtStatement extends Statement{
+public class WorkAtStatement extends Statement {
 
-	@Override
-	public void execute(World world, Unit unit, List<int[]> selectedCubes,SourceLocation sourceLocation) {
-		// TODO Auto-generated method stub
-		
+	public WorkAtStatement(Expression position, SourceLocation sourceLocation) {
+		super(sourceLocation);
+		this.setPosition(position);
 	}
-
+	
+	//TODO fix lists
+	@Override
+	public void execute(World world, Unit unit, List<int[]> selectedCubes, SourceLocation sourceLocation) {
+		int[] position = (int[]) this.getPosition().evaluate(world, unit, selectedCubes.get(0), sourceLocation);
+		unit.workAt(position);
+	}
+	
+	public Expression getPosition() {
+		return this.position;
+	}
+	
+	private void setPosition(Expression position) throws IllegalArgumentException {
+		if (! (position instanceof CubePositionExpression))
+			throw new IllegalArgumentException();
+		this.position = position;
+	}
+	
+	private Expression position;
 }
